19f3871c89348a4afdd3ceb137e58f402de1c43f
KHALED MOHAMED FATHALLAH MOHAMED <THEKINGSTAR5002@GMAIL.COM>
2023-06-13 00:01:57 +0300

    AI-IS-NOW-responsing for POST MESSAGE !!   \(--)/

--- API/AI_ENGINE/app.py
+++ API/AI_ENGINE/app.py
@@ -1,28 +1,44 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import AI_ENGINE
-from AI_ENGINE import get_recommendations
-from AI_ENGINE import display_learning_resources
+from AI_ENGINE import get_recommendations, display_learning_resources
 
 app = Flask(__name__)
 
-"""
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    data = {'message': 'Hello from the API!'}
-    return jsonify(data)
-"""
-
 @app.route('/api/data1', methods=['GET'])
 def get_projects():
-    data = {'message_get_recommendations': get_recommendations(["CSS","HTML"])}
+    input_data = ["input1", "input2", "input3"]  # Replace with your actual input data
+    data = {'message_get_recommendations': get_recommendations(input_data)}
     return jsonify(data)
 
 
 @app.route('/api/data2', methods=['GET'])
 def get_videos():
-    data = {'message_display_learning_resources': display_learning_resources(["CSS","HTML"])}
+    input_data = ["input4", "input5", "input6"]  # Replace with your actual input data
+    data = {'message_display_learning_resources': display_learning_resources(input_data)}
     return jsonify(data)
 
+
+@app.route('/api/data1', methods=['POST'])
+def process_data_recommendations():
+    data = request.json
+    inputs = data.get('inputs', [])  # Retrieve the inputs from the JSON data
+    
+    # Call your function with the received inputs
+    result = get_recommendations(inputs)
+
+    response = {'result': result}
+    return jsonify(response)
+
+@app.route('/api/data2', methods=['POST'])
+def process_data_videos():
+    data = request.json
+    inputs = data.get('inputs', [])  # Retrieve the inputs from the JSON data
+    
+    # Call your function with the received inputs
+    result = display_learning_resources(inputs)
+
+    response = {'result': result}
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run()
