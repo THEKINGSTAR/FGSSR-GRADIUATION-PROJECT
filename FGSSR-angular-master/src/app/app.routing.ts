edcecad27f90b2588fbc8d182fa2a546a10c7d4f
Error 404 Systematic Company <ahmed_20171531@icloud.com>
2023-06-14 07:59:14 +0300

    Commit

--- FGSSR-angular-master/src/app/app.routing.ts
+++ FGSSR-angular-master/src/app/app.routing.ts
@@ -26,7 +26,7 @@ export const rootRouterConfig: Routes = [
   {
     path: "",
     component: AdminLayoutComponent,
-    canActivate: [AuthGuard],
+    // canActivate: [AuthGuard],
     children: [
       {
         path: "dashboard",
