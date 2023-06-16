7530121ef54a7467d85426407883d99dbebc5278
Mahmoud Mostafa Hegab <36459093+mahmoudmostafahegab@users.noreply.github.com>
2023-06-16 15:19:17 +0200

    Edit Sources

--- FGSSR-angular-master/src/app/views/dashboard/analytics/analytics.component.ts
+++ FGSSR-angular-master/src/app/views/dashboard/analytics/analytics.component.ts
@@ -130,6 +130,9 @@ export class AnalyticsComponent implements OnInit, AfterViewInit {
     this.initDailyTrafficChartBar(this.themeService.activatedTheme);
     this.initDoughNutPieOptions(this.themeService.activatedTheme);
   }
+  ResourceLink(){
+    this.route.navigate(['/communication/Sources']);
+  }
   TeamtLink(){
     this.route.navigate(['forms/wizard']);
   }
